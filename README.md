2a8a94550b5d7691cb294bfdb7731f62aa51c7f1
Djailla <bastien.vallet@gmail.com>
2016-05-29 20:09:16 +0200

    First drop for Grafana app

---
+++ README.md
@@ -0,0 +1,7 @@
+Grafana - NAS OS App
+==================================
+
+**Grafana** is an app to install on NASes running NAS OS (for Seagate or LaCie NAS).
+It's based on the [grafana](https://github.com/grafana/grafana) project.
+
+It's a web dashboard for all services
---
+++ com.djailla.grafana/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -m 755 -p /opt/grafana/
+install -m 755 /home/source/rc.local /etc
+
+apt-get update
+if [ $RAINBOW_ARCHITECTURE = "x86_64" ]
+then
+    cd /tmp
+    wget https://grafanarel.s3.amazonaws.com/builds/grafana_3.0.4-1464167696_amd64.deb
+    apt-get install -y adduser libfontconfig
+    dpkg -i grafana_3.0.4-1464167696_amd64.deb
+else
+    echo "ARM"
+fi
+
+exit 0
